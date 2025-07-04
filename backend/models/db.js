b0c645b5ae878c224b8250d6618e51551ec914ce
Aryan Yadav <115806023+aryan9190@users.noreply.github.com>
2025-07-04 20:29:03 +0530

    Update db.js

--- backend/models/db.js
+++ backend/models/db.js
@@ -5,8 +5,9 @@ let db;
 
 export async function initDB() {
     db = await open({filename:"./haclib.db", driver: sqlite3.Database});
-    await db.exec(`CREATE TABLE IF NOT EXISTS resources (
-        id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, type TEXT, author TEXT, tags TEXT, link TEXT, notes TEXT, status TEXT)`);
+    await db.exec(`CREATE TABLE IF NOT EXISTS resources (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, type TEXT, author TEXT, tags TEXT, link TEXT, notes TEXT, status TEXT)`);
+    await db.exec(`CREATE TABLE IF NOT EXISTS suggestions (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, type TEXT, author TEXT, tags TEXT, link TEXT, notes TEXT, status TEXT)`);
+
 }
 
 export function getDB() {
